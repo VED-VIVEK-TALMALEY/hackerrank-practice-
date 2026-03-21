aa543c72d0fed93adb1f5ae7535e2273de52e8e0
VED TALMALEY <vedwork21@gmail.com>
2026-03-22 00:54:29 +0530

    Create map.cpp

---
+++ map.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#include<map>
+#include<unordered_map>
+
+int main() {
+   map < string , int > m;
+   int a,b,i,j,f;
+   char textt;
+   cin>>f;
+   for(i=0;i<f;i++){
+    cin>>a>>textt;
+    
+    m.insert(make_pair(textt,b));
+   }
+
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
